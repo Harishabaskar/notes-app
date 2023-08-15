c4275905907982c343c8c72fce2ad1fd937ab798
Harishabaskar <harishabaskar2002@gmail.com>
2023-08-15 20:10:37 +0530

    notes page form element added

--- note_frontend/src/Pages/Notespage/Notespage.jsx
+++ note_frontend/src/Pages/Notespage/Notespage.jsx
@@ -1,18 +1,44 @@
 import "./Notespage.css";
+// import {useState} from "react";
 
 function Notespage() {
+	
+	
+	
+	const toggleForm = () => {
+		const form = document.querySelector(".overlay")
+		form.classList.toggle("hidden")
+	}
+
 	return (
 		<div className="notespage">
 			<div className="addbutton">
-				<button>Add</button>
+				<button onClick = {toggleForm}>Add</button>
 			</div>
+			<div className = "overlay hidden"  >
+				<div className="form" >
+					<label htmlFor="">Add Note</label>
+					<input
+						type="text"
+						name="title"
+						id="title"
+						placeholder="Title"
+					/>
 
-			<div className="form">
-                <label htmlFor="">Add Note</label>
-                <input type="text" name="title" id="title" placeholder = "Title"/>
-                
-                <textarea name="content" id="content"  cols="30" rows="10" placeholder="Type a note"></textarea>
-            </div>
+					<textarea
+						name="content"
+						id="content"
+						cols="30"
+						rows="10"
+						placeholder="Type a note"
+					></textarea>
+					<div className="form_btn">
+						<button>Add</button>
+						<button onClick = {toggleForm}>Cancel</button>
+					</div>
+					
+				</div>
+			</div>
 		</div>
 	);
 }
